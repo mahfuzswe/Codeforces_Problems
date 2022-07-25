ec7d4a4ac73da9f9d057a8dfea3fef27a1cad301
Invincible-21 <shantobd1969@gmail.com>
2022-07-25 08:30:57 +0600

    stay focused

---
+++ 1030A.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n, a, a1 = 0, i;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%d", &a);
+        a1 = a1 + a;
+    }
+    if (a1 == 0)
+    {
+        printf("EASY\n");
+    }
+    else
+    {
+        printf("HARD\n");
+    }
+    return 0;
+}
---
+++ 271A_Beautiful_Year.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i, j, m1, m2, m3, m4;
+
+    cin >> n;
+
+    for (j = n + 1; j <= 9012; j++)
+    {
+        i = j;
+
+        m1 = i % 10;
+        i = i / 10;
+        m2 = i % 10;
+        i = i / 10;
+        m3 = i % 10;
+        i = i / 10;
+        m4 = i % 10;
+
+        if (m1 != m2 && m1 != m3 && m1 != m4 && m2 != m3 && m2 != m4 && m3 != m4)
+        {
+            break;
+        }
+    }
+    cout << j << endl;
+
+    return 0;
+}
