c31ad8aaa208881887a8b09972f7b325cf2b09ed
Invincible-21 <shantobd1969@gmail.com>
2023-01-05 22:19:28 +0600

    updated

---
+++ 1768A_Greatest_Convex.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    int t; cin>>t;
+
+    while(t--){
+        ll n; cin>>n;
+
+        cout<<(n-1)<<endl;
+    }
+
+    return 0;
+}
+
