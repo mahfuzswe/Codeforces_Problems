39afa0aaeb56f2312bf3b23224ec912558223f37
Invincible-21 <shantobd1969@gmail.com>
2023-01-12 21:19:42 +0600

    updated

---
+++ 1542A_Odd_Ser.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--){
+		int n,cnt[2]={0};
+		cin>>n;
+		for(int i=1,x;i<=n*2;i++){
+            cin>>x,cnt[x%2]++;
+        }
+		if(cnt[0]==n){
+            cout<<"YES\n";
+        }else{
+            cout<<"NO\n";
+        } 
+
+	}
+
+	return 0;
+}
+
