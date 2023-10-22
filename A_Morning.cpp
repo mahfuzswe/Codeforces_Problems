181d895ef2308895efb3629df256f2e6f4096041
Invincible-21 <shantobd1969@gmail.com>
2023-10-23 02:51:38 +0600

    let's march ahead

---
+++ A_Morning.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define int long long
+#define double long double
+#define pb(e) push_back(e);
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define mp make_pair
+#define flp(i,a,b) for(int i=a; i<=b; i++)
+#define sq(a) (a)*(a)
+#define all(x) x.begin(),x.end()
+using namespace std;
+        
+void mahfuzswe(){
+   int t;
+   cin>>t;
+   while(t--){
+     string s; cin>>s;
+     if(s[0]==s[1] && s[1]==s[2] && s[2]==s[3]) cout<<4<<'\n';
+     else{
+        
+     }
+  }
+}
+        
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    mahfuzswe();
+    
+    return 0;
+}
