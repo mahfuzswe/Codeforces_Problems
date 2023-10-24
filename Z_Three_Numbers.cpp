46ab1575b02b49d4a2dd4a3878a830cf8d91eed3
Invincible-21 <shantobd1969@gmail.com>
2023-10-24 09:25:21 +0600

    let's march ahead

--- Z_Three_Numbers.cpp
+++ Z_Three_Numbers.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main(){
+    int k,s; cin>>k>>s;
 
+    int cnt = 0;
+    for(int i=0; i<=k; i++){
+        for(int j=0; j<=k; j++){
+            if(s-i-j >= 0 && s-i-j <= k){
+                cnt++;
+            }
+        }
+    }
+    cout<<cnt<<'\n';
 
     return 0;
 }
