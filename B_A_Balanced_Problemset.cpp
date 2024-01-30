39dbb47ca4c7a00734b7cd7db7a974d383b1abbe
Invincible-21 <shantobd1969@gmail.com>
2024-01-31 03:20:32 +0600

    ..

--- B_A_Balanced_Problemset.cpp
+++ B_A_Balanced_Problemset.cpp
@@ -1,38 +1,21 @@
 #include<bits/stdc++.h>
-#define int long long
-#define double long double
-#define pb(e) push_back(e);
-#define fi first
-#define se second
-#define pii pair<int, int>
-#define mp make_pair
-#define flp(i,a,b) for(int i=a; i<=b; i++)
-#define sq(a) (a)*(a)
-#define all(x) x.begin(),x.end()
 using namespace std;
-        
-int gcd(int a, int b){if(a%b==0) return b; else return gcd(b, a%b);}
-int lcm(int a, int b){return (a*b)/gcd(a,b);}
-        
-void mahfuzswe(){
-   int t;
-   cin>>t;
-   while(t--){
-     int x, n; cin >> x >> n;
-     int ans = x%n;
-     int y = x/n;
-     
-     if(x != (ans + y)) ans += 1;
 
-     cout << ans << '\n';
-  }
-}
-        
+#define int long long
+
 int32_t main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);	cout.tie(0);
-    
-    mahfuzswe();
-    
-    return 0;
+  int t; cin >> t;
+  while(t--){
+    int x, n; cin >> x >> n;
+    int ans = 1;
+    for(int i=1; i*i<=x; i++){
+      if(x%i == 0){
+        if(i>=n) ans = max(ans, x/i);
+        if(x/i >= n) ans = max(ans, i);
+      }
+    }
+    cout << ans << '\n';
+  }
+
+  return 0;
 }
