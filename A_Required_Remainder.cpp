851b05ed769830d664d446b719b1184b16671bc9
Invincible-21 <shantobd1969@gmail.com>
2024-05-13 01:35:58 +0600

    updated

--- A_Required_Remainder.cpp
+++ A_Required_Remainder.cpp
@@ -6,7 +6,10 @@ int32_t main(){
     int t; cin >> t;
     while(t--){
         int x, y, n; cin >> x >> y >> n;
+        int p = (n-y)/x;
+        int k = (p*x) + y;
         
+        cout << k << '\n';
     }
 
     return 0;
